ac312feb6bab428f7e68db54fb616a2f55bcd2aa
sihun <yangsh5106@gmail.com>
2024-08-01 03:56:16 +0900

    add : music offset check system

--- Script/MusicPlay.cs
+++ Script/MusicPlay.cs
@@ -5,18 +5,27 @@ using UnityEngine;
 public class MusicPlay : MonoBehaviour {
 
     [SerializeField] AudioClip clip;
-    AudioSource audiosrc;
+    static AudioSource audiosrc;
     [SerializeField] float offset;
     void Start() {
         audiosrc = GetComponent<AudioSource>();
         audiosrc.clip = clip;
+        offset = OptionManager.instance.GetMusicOffset();
         PlayMusic().Forget();
     }
 
     async UniTaskVoid PlayMusic() {
-        await UniTask.Delay(TimeSpan.FromSeconds(GameManager.instance.musicWaitTime - offset));
+        await UniTask.Delay(TimeSpan.FromSeconds(GameManager.instance.musicWaitTime + offset * 0.001f));
         audiosrc.Play();
     }
 
     public void SetOffset(float offset) { this.offset = offset * 0.001f; }
+
+    public static void PauseMusic() {
+        audiosrc.Pause();
+    }
+
+    public static void UnpauseMusic() {
+        audiosrc.UnPause();
+    }
 }
--- Script/OptionManager.cs
+++ Script/OptionManager.cs
@@ -7,12 +7,13 @@ using UnityEngine.UI;
 
 public class OptionManager : MonoBehaviour {
     public static OptionManager instance = null;
-    [SerializeField] float _musicOffset;
+    [SerializeField] int _musicOffset;
     [SerializeField] float _noteSpeed;
     [SerializeField] AudioClip _tickSound;
     [SerializeField] Button _offsetStartButton;
     [SerializeField] Button _quitButton;
     [SerializeField] TextMeshProUGUI _currentOffsetTM;
+    [SerializeField] TextMeshProUGUI _currentNoteSpeedTM;
     [SerializeField] List<TextMeshProUGUI> _offsetNumberTMList;
 
     AudioSource _audioSource;
@@ -25,18 +26,46 @@ public class OptionManager : MonoBehaviour {
             if (instance != this)
                 Destroy(this.gameObject);
         }
+        DontDestroyOnLoad(gameObject);
     }
     private void Start() {
         _audioSource = GetComponent<AudioSource>();
+        UpdateNoteSpeedUI();
     }
 
-    public void SetNoteSpeed(float noteSpeed) {
-        _noteSpeed = noteSpeed;
+    public void NoteSpeedUp() {
+        if (_noteSpeed <= 10.9f)
+            _noteSpeed += 0.1f;
+        UpdateNoteSpeedUI();
+    }
+    public void NoteSpeedUpDouble() {
+        if (_noteSpeed <= 10)
+            _noteSpeed += 1;
+        UpdateNoteSpeedUI();
     }
 
-    public void SetMusicOffset(float offset) {
-        _musicOffset = offset;
+
+    public void NoteSpeedDown() {
+        if (_noteSpeed >= 0.1f)
+            _noteSpeed -= 0.1f;
+        UpdateNoteSpeedUI();
     }
+    public void NoteSpeedDownDouble() {
+        if (_noteSpeed >= 1)
+            _noteSpeed -= 1;
+        UpdateNoteSpeedUI();
+    }
+
+    public void MusicOffsetUp() {
+        _musicOffset++;
+        UpdateCurrentOffsetUI();
+    }
+
+    public void MusicOffsetDown() {
+        _musicOffset--;
+        UpdateCurrentOffsetUI();
+    }
+
     public float GetNoteSpeed() { return _noteSpeed; }
 
     public float GetMusicOffset() { return _musicOffset; }
@@ -50,15 +79,16 @@ public class OptionManager : MonoBehaviour {
         for (i = 0; i < 4; i++) {
             _offsetNumberTMList[i].text = "";
         }
-        _currentOffsetTM.text = $"current offset : {_musicOffset}";
+        UpdateCurrentOffsetUI();
     }
 
     private async UniTaskVoid OffsetUniTask() {
+        InitializeOffsetList();
         _offsetStartButton.enabled = false;
         _quitButton.enabled = false;
-        int i, j;
-        float sum = 0, result = 0;
+        int i, j, result, sum = 0;
         for (i = 0; i < 4; i++) {
+            var timerStopTime = OffsetTimer();
             _audioSource.pitch = 1;
             for (j = 0; j < 3; j++) {
                 _audioSource.PlayOneShot(_tickSound);
@@ -66,14 +96,33 @@ public class OptionManager : MonoBehaviour {
             }
             _audioSource.pitch = 1.4f;
             _audioSource.PlayOneShot(_tickSound);
-            result += i;
+            result = (int)MathF.Floor((1.5f - await timerStopTime) * 1000);
+
             _offsetNumberTMList[i].text = $"{result}";
             sum += result;
             await UniTask.Delay(TimeSpan.FromSeconds(0.5f));
         }
         _musicOffset = sum / 4;
-        _currentOffsetTM.text = $"current offset : {_musicOffset}";
+        UpdateCurrentOffsetUI();
         _offsetStartButton.enabled = true;
         _quitButton.enabled = true;
     }
+
+    private async UniTask<float> OffsetTimer() {
+        float timer = 0;
+        while (!Input.GetKeyDown(KeyCode.Space)) {
+            timer += Time.deltaTime;
+            await UniTask.NextFrame();
+        }
+        return timer;
+    }
+
+    private void UpdateCurrentOffsetUI() {
+        _currentOffsetTM.text = $"current offset : {_musicOffset}";
+    }
+
+    private void UpdateNoteSpeedUI() {
+        _currentNoteSpeedTM.text = $"current notespeed : {_noteSpeed}";
+    }
+
 }
